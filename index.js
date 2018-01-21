2d0bd43ae1b19cd26ea5545351ebe114b304e5db
godspeedelbow <eelke@boezeman.com>
2018-01-21 22:33:07 +0100

    fixed babel

--- index.js
+++
@@ -1,6 +0,0 @@
-// source: https://medium.com/the-graphqlhub/your-first-graphql-server-3c766ab4f0a2
-
-// index.js
-// by requiring `babel-register`, all of our successive `require`s will be Babel'd
-require('babel-register');
-require('./server.js');
